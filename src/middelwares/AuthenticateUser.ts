59e1dd75ef40698fb02931301b3616eb200689af
elhajjaji.riad <riad@gmail.com>
2023-06-04 16:34:14 -0400

    ++

--- src/middelwares/AuthenticateUser.ts
+++ src/middelwares/AuthenticateUser.ts
@@ -1,26 +1,36 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+
 dotenv.config();
 
-
 interface CustomRequest extends Request {
-    user?: any;
-  }
-  
-  
-const AuthenticateUser = (req:CustomRequest, res:Response, next:NextFunction) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+  user?: unknown;
+}
 
-  if (!token) {
-    return res.status(403).send("A token is required for authentication");
+const AuthenticateUser = (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+): Response<unknown, Record<string, unknown>> | void => {
+  let token: string | undefined;
+
+  if (typeof req.body.token === 'string') {
+    token = req.body.token;
+  } else if (typeof req.query.token === 'string') {
+    token = req.query.token;
+  } else {
+    token = req.headers['x-access-token'] as string | undefined;
+  }
+
+  if (token == null) {
+    return res.status(403).send('A token is required for authentication');
   }
   try {
     const decoded = jwt.verify(token, `${process.env.TOKEN_KEY}`);
     req.user = decoded;
   } catch (err) {
-    return res.status(401).send("Invalid Token");
+    return res.status(401).send('Invalid Token');
   }
   return next();
 };
